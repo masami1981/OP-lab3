c590f6f49002b7d5d39026e3d07dfe5ebc8e58d5
Marko Samirić <marko.samiric@gmail.com>
2014-03-31 22:45:37 +0200

    everything fixed - classes fixed - added class cSluzbenik - FIFO creation and *zahtjev deletion

--- README.md
+++ README.md
@@ -1,24 +1,34 @@
 3. Laboratorijska vježba
 Pokazivači i reference
 
-Osmisliti i izraditi sustav za vođenje procesa legalizacije nelegalno izgrađenih građevina.
-Sustav funkcionira na način da korisnici prvo dostave zahtjev za legalizacijom zajedno s
-pripadajućom dokumentacijom (geodetska podloga, arhitektonski elaborat i statički proračun).
-Predmeti se rješavaju na način da 5 službenika uzimaju predmete redoslijedom zaprimanja, te
-ih obrađuju i prebacuju u red za rješene predmete. Obrada se obavlja na način da se na
-primljenom zahjevu postavlja marker „OK“, ukoliko je dostavljena cjelokupna dokumentacija.
+Osmisliti i izraditi sustav za vođenje procesa
+legalizacije nelegalno izgrađenih građevina.Sustav
+funkcionira na način da korisnici prvo dostave
+zahtjev za legalizacijom zajedno s pripadajućom
+dokumentacijom (geodetska podloga, arhitektonski
+elaborat i statički proračun).
+Predmeti se rješavaju na način da 5 službenika
+uzimaju predmete redoslijedom zaprimanja, te ih
+obrađuju i prebacuju u red za rješene predmete.
+Obrada se obavlja na način da se na primljenom zahjevu
+postavlja marker „OK“, ukoliko je dostavljena cjelokupna
+dokumentacija.
 
 Domaći rad
 Izraditi klasu koja predstavlja zahtjev za legalizacijom.
-Izraditi klasu koja predstavlja FIFO red zahtjeva za legalizaciju implementiran uz pomoć
-jednostruko vezane liste. Svaki element liste treba sadržavati pokazivač na zahtjev za
-legalizacijom.
+
+Izraditi klasu koja predstavlja FIFO red zahtjeva za
+legalizaciju implementiran uz pomoć jednostruko vezane
+liste. Svaki element liste treba sadržavati pokazivač na
+zahtjev za legalizacijom.
 
 Zadatak za laboratorijske vježbe
-Izraditi klasu koja će obavljati posao službenika za obradu zahtjeva za legalizacijom. Metoda
-za obradu zahtjeva treba primati referencu na zahtjev kojeg treba obraditi.
+Izraditi klasu koja će obavljati posao službenika za
+obradu zahtjeva za legalizacijom. Metoda za obradu zahtjeva
+treba primati referencu na zahtjev kojeg treba obraditi.
 
-Napraviti glavni program i demonstrirati rad sustava uz pomoć izbornika.
+Napraviti glavni program i demonstrirati rad sustava uz
+pomoć izbornika.
 1. Novi zahtjev (ime, prezime, adresa, dokumentacija (oznaka))
 2. Obradi zahtjev (indeks službenika)
 3. Ispisi sadržaj reda rješenih predmeta
--- main.cpp
+++ main.cpp
@@ -1,35 +1,67 @@
 #include <iostream>
 #include <string>
+#define BR_SLUZ 5
 
 using namespace std;
 
 class cZahtjev{
 private:
+    bool geo;
+    bool arh;
+    bool stat;
     bool dok_ok;
     string ime;
     string prezime;
     string adresa;
+    int sluzbenik;
 public:
-    cZahtjev(string ime, string prez, string adr, bool dok){
+    cZahtjev(){};
+    cZahtjev(string ime, string prez, string adr, bool geo, bool arh, bool stat){
         this->ime=ime;
         this->prezime=prez;
         this->adresa=adr;
-        this->dok_ok=dok;
+        this->geo=geo;
+        this->arh=arh;
+        this->stat=stat;
     }
     string get_zahtjev(){
-        return (ime+" "+prezime+", "+adresa+". "+(dok_ok ? "Dokumentacija OK.":"Nedostaje dokumentacija."));
+        if(this != NULL) return (ime+" "+prezime+", "+adresa+". "+(dok_ok ? "Dokumentacija OK.":"Nedostaje dokumentacija."));
+        else return "nema zahtjeva";
+    }
+    bool get_dokumentacija(){
+        return ((geo && arh && stat) ? true : false);
+    }
+    bool set_dok_ok(int sluz){
+        if(geo && arh && stat){
+            this->sluzbenik = sluz;
+            return this->dok_ok=true;
+        }
+        else return false;
+    }
+};
+
+class cSluzbenik{
+    int sluzbenik;
+public:
+    void set_sluz(int sluz){
+        this->sluzbenik = sluz;
+    }
+    int get_sluz(){
+        return this->sluzbenik;
     }
 
+    bool ObradaZahtjeva(cZahtjev * zahtjev){
+        return zahtjev->set_dok_ok();
+    }
 };
 
 class cLista{
     cZahtjev * zahtjev;
     cLista * next;
-    string sluzbenik;
 public:
-    void DodajNoviZahtjev(string ime, string prez, string adr, bool dok){
+    void SpremiZahtjev(cZahtjev * zahtjev){
         cLista * tmp1 = new cLista;
-        tmp1->zahtjev =  new cZahtjev(ime, prez, adr, dok);
+        tmp1->zahtjev =  zahtjev;
         tmp1->next = NULL;
         cLista * tmp2 = new cLista;
         tmp2 = this;
@@ -44,23 +76,78 @@ public:
             tmp2->next = tmp1;
         }
     }
+
+    cZahtjev * DohvatiZahtjev(){
+        if(this->zahtjev == NULL) return NULL;
+        else {
+            cZahtjev * tmp = this->zahtjev;
+            if(this->next != NULL){
+                this->zahtjev = this->next->zahtjev;
+                this->next = this->next->next;
+            }
+            else{
+                this->zahtjev = NULL;
+            }
+            return tmp;
+        }
+    }
+
     ~cLista(){
-        delete(zahtjev);
-        delete(next);
+        while(this->next != NULL){
+            delete zahtjev;
+            zahtjev = NULL;
+            this->zahtjev = this->next->zahtjev;
+            this->next = this->next->next;
+        }
+        delete zahtjev;
+        zahtjev = NULL;
     }
 };
 
 int main()
 {
-    cLista * lista = new cLista;
+    cZahtjev * zahtjev;
+    cLista * neobradeni = new cLista;
+    cLista * obradeni = new cLista;
+    cSluzbenik sluzbenik[BR_SLUZ];
+    for(int i=0; i<BR_SLUZ ; i++)
+        sluzbenik[i].set_sluz(i+1);
+    zahtjev = new cZahtjev("hbjkhb", "7trfzubj", "bla", true, true, true);
+    neobradeni->SpremiZahtjev(zahtjev);
+    zahtjev = new cZahtjev("34sgs553", "53255dsfgf", "bla", true, true, true );
+    neobradeni->SpremiZahtjev(zahtjev);
+    zahtjev = new cZahtjev("1324", "214dwqwd", "bl64a", true, true, true);
+    neobradeni->SpremiZahtjev(zahtjev);
+    zahtjev = new cZahtjev("67568", "arweatr", "6wqbla", true, true, true);
+    neobradeni->SpremiZahtjev(zahtjev);
+    zahtjev = new cZahtjev("1324we64", "afawr", "rqw", true, true, true);
+    neobradeni->SpremiZahtjev(zahtjev);
+    zahtjev = new cZahtjev("1w534q324", "fas", "dfgsabla", true, true, true);
+    neobradeni->SpremiZahtjev(zahtjev);
 
-    lista->DodajNoviZahtjev("hbjkhb", "7trfzubj", "bla", true);
-    lista->DodajNoviZahtjev("34sgs553", "53255dsfgf", "bla", true);
-    lista->DodajNoviZahtjev("1324", "214dwqwd", "bl64a", true);
-    lista->DodajNoviZahtjev("67568", "arweatr", "6wqbla", true);
-    lista->DodajNoviZahtjev("1324we64", "afawr", "rqw", true);
-    lista->DodajNoviZahtjev("1w534q324", "fas", "dfgsabla", true);
-    cout << "Hello World!" << endl;
+
+
+    /*
+    zahtjev = neobradeni->DohvatiZahtjev();
+    cout << zahtjev->get_zahtjev() << endl;
+    zahtjev = neobradeni->DohvatiZahtjev();
+    cout << zahtjev->get_zahtjev() << endl;
+    zahtjev = neobradeni->DohvatiZahtjev();
+    cout << zahtjev->get_zahtjev() << endl;
+    zahtjev = neobradeni->DohvatiZahtjev();
+    cout << zahtjev->get_zahtjev() << endl;
+    zahtjev = neobradeni->DohvatiZahtjev();
+    cout << zahtjev->get_zahtjev() << endl;
+    zahtjev = neobradeni->DohvatiZahtjev();
+    cout << zahtjev->get_zahtjev() << endl;
+    zahtjev = neobradeni->DohvatiZahtjev();
+    cout << zahtjev->get_zahtjev() << endl;*/
+
+
+    delete neobradeni;
+    neobradeni = NULL;
+    delete zahtjev;
+    zahtjev = NULL;
     return 0;
 }
 
